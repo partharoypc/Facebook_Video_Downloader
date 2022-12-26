3c6f8028aace23f52deb8133e0ae6c13dd59ef8b
Partha Roy <partharoypc@gmail.com>
2022-12-26 13:35:40 +0600

    Initial commit

---
+++ app/src/main/java/com/sikderithub/facebookvideodownloader/MainActivity.java
@@ -0,0 +1,143 @@
+package com.sikderithub.facebookvideodownloader;
+
+import static android.content.ClipDescription.MIMETYPE_TEXT_PLAIN;
+import static com.sikderithub.facebookvideodownloader.Utils.createFileFolder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "";
+    EditText txtLink;
+    TextView btnDownloaded,btnPest;
+    private ClipboardManager clipBoard;
+    private MainActivity activity;
+    private String videoUrl;
+    private String strName = "facebook";
+    private String strNameSecond = "fb";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtLink = findViewById(R.id.link_text);
+        btnDownloaded = findViewById(R.id.downloaded_btn);
+        btnPest = findViewById(R.id.btn_paste);
+
+
+        initViews();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        activity = this;
+        clipBoard = (ClipboardManager) activity.getSystemService(CLIPBOARD_SERVICE);
+        pasteText();
+    }
+
+    private void initViews() {
+        clipBoard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        btnPest.setOnClickListener(view -> {
+            pasteText();
+        });
+
+        btnDownloaded.setOnClickListener(v -> {
+            String ll = txtLink.getText().toString();
+            if (ll.equals("")) {
+                Utils.setToast(activity, getResources().getString(R.string.enter_url));
+            } else if (!Patterns.WEB_URL.matcher(ll).matches()) {
+                Utils.setToast(activity, getResources().getString(R.string.enter_valid_url));
+            } else {
+                getFacebookData();
+            }
+        });
+    }
+
+    private void pasteText() {
+        try {
+            txtLink.setText("");
+            String copyIntent = getIntent().getStringExtra("CopyIntent");
+            if (copyIntent == null || copyIntent.equals("")) {
+                if (!(clipBoard.hasPrimaryClip())) {
+                    Log.d(TAG, "pasteText: PasteText");
+                } else if (!(clipBoard.getPrimaryClipDescription().hasMimeType(MIMETYPE_TEXT_PLAIN))) {
+
+                    if (clipBoard.getPrimaryClip().getItemAt(0).getText().toString().contains(strName)) {
+                        txtLink.setText(clipBoard.getPrimaryClip().getItemAt(0).getText().toString());
+                    } else if (clipBoard.getPrimaryClip().getItemAt(0).getText().toString().contains(strNameSecond)) {
+                        txtLink.setText(clipBoard.getPrimaryClip().getItemAt(0).getText().toString());
+                    }
+
+                } else {
+                    ClipData.Item item = clipBoard.getPrimaryClip().getItemAt(0);
+                    if (item.getText().toString().contains(strName)) {
+                        txtLink.setText(item.getText().toString());
+                    } else if (item.getText().toString().contains(strNameSecond)) {
+                        txtLink.setText(item.getText().toString());
+                    }
+
+                }
+            } else {
+                if (copyIntent.contains(strName)) {
+                    txtLink.setText(copyIntent);
+                } else if (copyIntent.contains(strNameSecond)) {
+                    txtLink.setText(copyIntent);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void getFacebookData() {
+        try {
+            createFileFolder();
+            URL url = new URL(txtLink.getText().toString());
+
+            String host = url.getHost();
+            if (host.contains(strName) || host.contains(strNameSecond)) {
+                Utils.showProgressDialog(activity);
+            } else {
+                Utils.setToast(activity, getResources().getString(R.string.enter_valid_url));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == 100 && resultCode == RESULT_OK) {
+                getFacebookUserData();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void getFacebookUserData() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/sikderithub/facebookvideodownloader/Utils.java
@@ -0,0 +1,91 @@
+package com.sikderithub.facebookvideodownloader;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class Utils {
+    public static Dialog customDialog;
+    private  Context context;
+
+
+    public static String RootDirectoryFacebook = "/FacebookVideoDownloader/";
+    public static File RootDirectoryFacebookShow = new File(Environment.getExternalStorageDirectory() + "/Download/FacebookVideoDownloader");
+
+    public Utils(Context mContext) {
+        context = mContext;
+    }
+
+    public static void setToast(Context mContext, String str) {
+        Toast toast = Toast.makeText(mContext, str, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    public static void createFileFolder() {
+        if (!RootDirectoryFacebookShow.exists()) {
+            RootDirectoryFacebookShow.mkdirs();
+        }
+    }
+
+    public static void showProgressDialog(Activity activity) {
+        System.out.println("Show");
+        if (customDialog != null) {
+            customDialog.dismiss();
+            customDialog = null;
+        }
+        customDialog = new Dialog(activity);
+        LayoutInflater inflater = LayoutInflater.from(activity);
+        View mView = inflater.inflate(R.layout.progress_dialog, null);
+        customDialog.setCancelable(false);
+        customDialog.setContentView(mView);
+        if (!customDialog.isShowing() && !activity.isFinishing()) {
+            customDialog.show();
+        }
+    }
+
+    public static void hideProgressDialog(Activity activity) {
+        System.out.println("Hide");
+        if (customDialog != null && customDialog.isShowing()) {
+            customDialog.dismiss();
+        }
+    }
+
+    public static void startDownload(String downloadPath, String destinationPath, Context context, String FileName) {
+        setToast(context, context.getResources().getString(R.string.download_started));
+        Uri uri = Uri.parse(downloadPath);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setTitle(FileName+"");
+        request.setVisibleInDownloadsUi(true);
+        request.setDestinationInExternalPublicDir(DIRECTORY_DOWNLOADS,destinationPath+FileName);
+        ((DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE)).enqueue(request);
+
+        try {
+            MediaScannerConnection.scanFile(context, new String[]{new File(DIRECTORY_DOWNLOADS + "/" + destinationPath + FileName).getAbsolutePath()},
+                    null, new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
